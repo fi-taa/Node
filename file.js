5a301a81ad11dc0f42f1b2ebba4ec32f852910d5
Fita-Wegene <fitaawd@gmail.com>
2023-02-21 22:34:45 +0300

    file.js and blog.txt added

---
+++ file.js
@@ -0,0 +1,17 @@
+//file system
+const fs = require('fs');
+
+//read file system
+// fs.readFile('./blog.txt', (err,data)=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log(data.toString());
+//     }
+// })
+
+// write file system
+
+// fs.writeFile('./blog.txt',"nodes world",()=>{
+//     console.log("Done!");
+// })
