d134640072e4982f330b1e81fd4d774a751488b6
Fita-Wegene <fitaawd@gmail.com>
2023-02-24 01:01:03 +0300

    view engine 'ejs' example

--- app.js
+++ app.js
@@ -4,15 +4,20 @@ const express = require('express');
 const app = express();
 
 app.listen('3000')
-
+app.set('view engine', 'ejs')
 app.get('/', (req, res)=>{
-    res.sendFile('./views/home.html',{root  : __dirname })
+    // res.sendFile('./views/home.html',{root  : __dirname })
+    res.render('index')
 })
 
 app.get('/about', (req, res)=>{
-    res.sendFile('./views/about.html',{root  : __dirname })
+    // res.sendFile('./views/about.html',{root  : __dirname })
+    res.render('about')
 })
-
+app.get('/blog/create', (req, res)=>{
+    res.render('create')
+});
 app.use((req, res)=>{
-    res.status(404).sendFile('./views/404.html',{root : __dirname})
+    // res.status(404).sendFile('./views/404.html',{root : __dirname})
+    res.render('404')
 })
